3f9a4fff93daa00bcbd96ffb40f26bc1f853fc97
Marios Titas <redneb@gmx.com>
2025-05-10 03:29:42 +0300

    adjust wrapping modes list order and descriptions & change default mode

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
 export type WrappingMode =
-	| "block"
+	| "strict_arrow_function"
 	| "function"
 	| "arrow_function"
+	| "block"
 	| "raw"
 
-export const default_wrapping_mode: WrappingMode = "block";
+export const default_wrapping_mode: WrappingMode = "strict_arrow_function";
 
 export interface WrappingModeInfo {
 	description: string;
@@ -15,23 +16,29 @@ export interface WrappingModeInfo {
 }
 
 export const wrapping_modes: Record<WrappingMode, WrappingModeInfo> = {
-	block: {
-		description: "Block (recommended)",
-		url_param: "b",
-		parse_regex: /^\s*\{(?:[\t ]*\n)?(.*?)(?:\n[\t ]*)?\}\s*$/s,
-		wrap: code_text => `{\n${code_text}\n}`,
+	strict_arrow_function: {
+		description: "Arrow IIFE (strict mode)",
+		url_param: "s",
+		parse_regex: /^\s*\(\s*\(\s*\)\s*=>\s*\{\s*(?:"use strict"|'use strict');?(?:[\t ]*\n)?(.*?)(?:\n[\t ]*)?\}\s*\)\s*\(\s*\)\s*$/s,
+		wrap: code_text => `(()=>{"use strict";\n${code_text}\n})()`,
 	},
 	function: {
-		description: "Function (for older browsers)",
+		description: "IIFE (for older browsers)",
 		url_param: "f",
 		parse_regex: /^\s*\(\s*function\s*\(\s*\)\s*\{(?:[\t ]*\n)?(.*?)(?:\n[\t ]*)?\}\s*\)\s*\(\s*\)\s*$/s,
 		wrap: code_text => `(function(){\n${code_text}\n})()`,
 	},
 	arrow_function: {
-		description: "Arrow function",
+		description: "Arrow IIFE",
 		url_param: "a",
 		parse_regex: /^\s*\(\s*\(\s*\)\s*=>\s*\{(?:[\t ]*\n)?(.*?)(?:\n[\t ]*)?\}\s*\)\s*\(\s*\)\s*$/s,
 		wrap: code_text => `(()=>{\n${code_text}\n})()`,
+	},
+	block: {
+		description: "Block",
+		url_param: "b",
+		parse_regex: /^\s*\{(?:[\t ]*\n)?(.*?)(?:\n[\t ]*)?\}\s*$/s,
+		wrap: code_text => `{\n${code_text}\n}`,
 	},
 	raw: {
 		description: "No wrapping, raw code",
